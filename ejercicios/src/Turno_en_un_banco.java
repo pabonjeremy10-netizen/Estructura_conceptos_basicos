f6e43e10642e236d72c0da0117c9c72c54f79a3a
pabonjeremy10-netizen <pabonjeremy10@gmail.com>
2026-03-02 07:08:42 -0500

    Update Turno_en_un_banco.java

--- ejercicios/src/Turno_en_un_banco.java
+++ ejercicios/src/Turno_en_un_banco.java
@@ -1,3 +1,9 @@
+//ejercicio 6
+//Turno en un banco
+//Un cajero de banco trabaja con el número del turno actual. Se crea una variable turnoActual y se asigna a turnoEnPantalla.
+//Demuestra con código que cambiar turnoEnPantalla no afecta a turnoActual (comportamiento por valor).
+//Explica en un comentario por qué ocurre esto.
+
 public class Turno_en_un_banco {
     public static void main(String[] args) {
 
