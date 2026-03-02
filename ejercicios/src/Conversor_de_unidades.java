4e10843eb7ff341a3c6e2abaf7f5c127a1985f99
pabonjeremy10-netizen <pabonjeremy10@gmail.com>
2026-03-02 07:07:32 -0500

    Update Conversor_de_unidades.java

--- ejercicios/src/Conversor_de_unidades.java
+++ ejercicios/src/Conversor_de_unidades.java
@@ -1,3 +1,11 @@
+//ejercicio 5
+// Conversor de unidades
+//Una aplicación de cocina necesita convertir unidades:
+//Declara una variable tazas (double) con la cantidad de tazas de un ingrediente.
+//Convierte a mililitros (1 taza = 236.588 ml) y guarda el resultado en otra variable.
+//Convierte también a litros.
+//Muestra las tres medidas por consola.
+
 public class Conversor_de_unidades {
     
      public static void main(String[] args) {
