b3f0e5afc722292b52421f683886a6656c31705d
pabonjeremy10-netizen <pabonjeremy10@gmail.com>
2026-03-02 07:02:32 -0500

    Update calculadora_de_una_tienda.java

--- ejercicios/src/calculadora_de_una_tienda.java
+++ ejercicios/src/calculadora_de_una_tienda.java
@@ -1,3 +1,11 @@
+//Calcuadora de una tienda
+// tienda de barrio vende productos al por menor. El cajero necesita calcular:
+//Precio unitario de un producto (double)
+//Cantidad comprada (int)
+//Descuento aplicado en porcentaje (double)
+//Total a pagar después del descuento
+//Declara las variables, realiza el cálculo y muestra el resultado por consol
+
 public class calculadora_de_una_tienda {
 
     public static void main(String[] args) {
