d52d2916efd7fc3f5d466ce2b85bc11e511699b1
pabonjeremy10-netizen <pabonjeremy10@gmail.com>
2026-03-02 07:14:33 -0500

    Update Invertario_compartido.java

--- ejercicios/src/Invertario_compartido.java
+++ ejercicios/src/Invertario_compartido.java
@@ -1,36 +1,46 @@
-public class Invertario_compartido {
+/*ejercicio 7
+Inventario compartido Un almacén tiene un arreglo con el stock de 5 productos.
+El sistema de ventas y el sistema de reportes apuntan al mismo arreglo.
+Crea el arreglo stockAlmacen con valores iniciales. 
+Asigna stockReportes = stockAlmacen. Simula una venta reduciendo el stock de un producto desde stockReportes. 
+Muestra que stockAlmacen también cambió y explica en comentarios por qué.
+*/
+    public class Inventario_Compartido {
 
     public static void main(String[] args) {
 
-        int[] stock = {10, 20, 15, 8, 30};
+        int[] stockAlmacen = {50, 30, 20, 15, 10};
 
-        int[] stockReportes = stock;
+        int[] stockReportes = stockAlmacen;
 
-        stockReportes[1] = stockReportes[1] - 5;
+        System.out.println("Stock inicial:");
+        mostrarStock(stockAlmacen);
 
-        System.out.println("Stock desde almacén:");
-        for (int i = 0; i < stock.length; i++) {
-            System.out.println("Producto " + i + ": " + stock[i]);
-        }
+        stockReportes[1] -= 5;
 
-        System.out.println("\nStock desde reportes:");
-        for (int i = 0; i < stockReportes.length; i++) {
-            System.out.println("Producto " + i + ": " + stockReportes[i]);
-        }
+        System.out.println("\nDespués de la venta (modificado desde stockReportes):");
+
+        System.out.println("Stock Almacén:");
+        mostrarStock(stockAlmacen);
+
+        System.out.println("Stock Reportes:");
+        mostrarStock(stockReportes);
 
         /*
-         Aquí sí cambia todo porque los arreglos en Java NO se copian como los int.
-         Cuando hacemos:
-         
-         int[] stockReportes = stockAlmacen;
-         
-         No se crea un arreglo nuevo.
-         Los dos están apuntando al mismo arreglo en memoria.
-         
-         Entonces cuando cambiamos stockReportes[1],
-         en realidad estamos cambiando el mismo arreglo que usa stockAlmacen.
-         
-         O sea, no hay copia… están compartiendo la misma cosa.
+         EXPLICACIÓN:
+         stockReportes = stockAlmacen NO crea una copia del arreglo.
+         Ambos apuntan al mismo espacio de memoria.
+
+         En Java, los arreglos son tipos por referencia.
+         Por eso, cuando se modifica stockReportes,
+         también cambia stockAlmacen automáticamente.
         */
+    }
+
+    public static void mostrarStock(int[] stock) {
+        for (int cantidad : stock) {
+            System.out.print(cantidad + " ");
+        }
+        System.out.println();
     }
 }
